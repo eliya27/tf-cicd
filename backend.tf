c8cad17a1a5edf5713e6fb09c22e2e537b6fff78
eliya27 <57147052+eliya27@users.noreply.github.com>
2022-05-20 17:45:02 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     storage_account_name = "gervas95121"
     container_name = "terraform-state"
     key = "terraform.tfstate"
-    access-key = "?sv=2017-07-29&ss=b&srt=sco&sp=rwdlac&se=2024-05-16T13:57:45Z&st=2022-05-17T13:57:45Z&spr=https&sig=Gidv8U3e6NHMQLp5oMHiWM9BsiEERdomy6aDXP5QHkw%3D"
+    access_key = "?sv=2017-07-29&ss=b&srt=sco&sp=rwdlac&se=2024-05-16T13:57:45Z&st=2022-05-17T13:57:45Z&spr=https&sig=Gidv8U3e6NHMQLp5oMHiWM9BsiEERdomy6aDXP5QHkw%3D"
 
   }
 }
