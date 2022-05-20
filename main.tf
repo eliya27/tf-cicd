094472c8924a0723057bca3ba27b2929a8de1c1e
eliya27 <57147052+eliya27@users.noreply.github.com>
2022-05-20 10:54:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ terraform {
     required_providers  {
         azurerm =   {
             source  =   "hashicorp/azurerm"
+            version         =   "=2.99.0"
         }
     }
 }
@@ -15,7 +16,7 @@ terraform {
 # Provider Block
 
 provider "azurerm" {
-    version         =   "=2.99.0"
+    
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
