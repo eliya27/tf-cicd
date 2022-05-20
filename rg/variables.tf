6fc81c4f1faa124e4f349d74d55ee5d4a55a6481
eliya27 <57147052+eliya27@users.noreply.github.com>
2022-05-20 13:48:22 +0300

    Update variables.tf

--- rg/variables.tf
+++ rg/variables.tf
@@ -2,8 +2,10 @@
 variable "resource_group_name" {
     description = "Resource group name"
     type = string
+    default = "jenkins-pipers"
 }
 
 variable "resource_group_location" {
     description = "Rs location"
     type = string
+    default = "eastus2"
