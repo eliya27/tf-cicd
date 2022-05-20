f1c71d7209520abb64f01ce370de02114f644fe7
Eliya Gervas <eliyagervas27@gmail.com>
2022-05-20 16:29:57 +0300

    new files added

---
+++ azure/rg-module/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "resource group name"
+  type = string
+  default = "new-pipeline-rs"
+}
+
+variable "resource_group_location" {
+  description = "resource group location to be created"
+  type = string
+  default = "southafricanorth"
+}
--- variables.tf
+++ variables.tf
@@ -24,14 +24,3 @@ variable "tenant_id" {
     type        =   string
 }
 
-variable "resource_group_name" {
-    description = "Resource group name"
-    type = string
-    default = "jenkins-pipeliners"
-}
-
-variable "resource_group_location" {
-    description = "Rs location"
-    type = string
-    default = "eastus2"
-}
